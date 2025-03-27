fe23c81dc1a2eab45c1c24ab336d8b6572180f5c
pjm529 <pjm529529@gmail.com>
2025-03-27 11:21:25 +0900

    Chore: init

---
+++ src/test/java/com/sparta/ch3schedulerjpa/Ch3SchedulerJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.ch3schedulerjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch3SchedulerJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
